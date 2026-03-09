7fd1fcfba2950250553ee424d6596b2f4dae7719
Sanjay Satheesh <sanjaysatheesh66@gmail.com>
2026-03-09 15:43:48 +0530

    Add files via upload

---
+++ details.sh
@@ -0,0 +1,11 @@
+echo "Enter your name:"
+read Name
+echo "Enter program name:"
+read Prog
+echo "Enter your enrollment number:"
+read Enroll
+clear
+echo "Details you entered"
+echo Name: $Name
+echo Program Name: $Prog 
+echo Enrollment Number: $Enroll
